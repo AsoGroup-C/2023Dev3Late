f9406e909a4abdb1b996ab70c18178480f769a01
tarotarotarot <tarotou1018@gmail.com>
2023-11-21 21:12:27 +0900

    都道府県全表示

---
+++ 04_ソースコード/back/dao/prefecturesDAO.php
@@ -0,0 +1,56 @@
+<?php
+//JSON形式で返却すること、文字形式がUTF-8だということの宣言
+
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=UTF-8');
+class select_main
+{
+    function dbconnect()
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=traveroulette;charset=utf8','webuser','abccsd2');
+    
+        return $pdo;
+    }
+    
+    //都道府県データ取得
+    function get_prefectures($area_id)
+    {
+        $pdo = $this->dbconnect();
+        $sql = 'SELECT prefecture_name FROM prefectures WHERE area_id = ?';
+        $ps = $pdo->prepare($sql);
+        $ps->bindValue(1, $area_id, PDO::PARAM_INT);
+        $ps->execute();
+        $prefecture_name = $ps->fetchAll();
+    
+        //配列の宣言（無いとエラーが発生した）
+        $pre_data = array();
+    
+        //データベースから持ってきたデータをforeachを利用してデータの数だけ$com_dataに追加している
+        foreach ($prefecture_name as $row) {
+            array_push($pre_data, array(
+                'prefecture_name' => $row['prefecture_name']
+            ));
+        }
+        //arrayの中身をJSON形式に変換している
+        $json_array = json_encode($pre_data);
+    
+        return $json_array;
+    }
+    
+    function get_prefecturesArray()
+    {
+        $pre_array = array();
+        
+        for($i = 1;$i <= 7;$i++){
+            array_push($pre_array, array(
+                'prefectures' => $this->get_prefectures($i)
+            ));
+        }
+    
+        $json_array = json_encode($pre_array);
+    
+        print $json_array;
+    }
+}
+
+?>
--- 04_ソースコード/front/script/script.js
+++ 04_ソースコード/front/script/script.js
@@ -1,34 +1,70 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        regions:{
-          hokkaido:{
-            title:"北海道・東北",
-            sample:["北海道","青森県","秋田県","長野県","岩手県"],
-            isFaded: true,
-            buttonColor: "#99CCFF",
-          },
-          kanto:{
-            title:"関東",
-            sample:["茨城県","栃木県","群馬県","埼玉県","千葉県"],
-            isFaded: true,
-            buttonColor: "#99CC99",
-          },
-        },
+  el: '#app',
+  data: {
+    regions: {
+      hokkaido: { title: "北海道・東北", prefectures: [], isFaded: true, buttonColor: "#99CCFF" },
+      kanto: { title: "関東", prefectures: [], isFaded: true, buttonColor: "#CCFFFF" },
+      tyubu: { title: "中部", prefectures: [], isFaded: true, buttonColor: "#99CC99" },
+      kinki: { title: "近畿", prefectures: [], isFaded: true, buttonColor: "#FFCC66" },
+      tyugoku: { title: "中国", prefectures: [], isFaded: true, buttonColor: "#FF9966" },
+      shikoku: { title: "四国", prefectures: [], isFaded: true, buttonColor: "#CC6666" },
+      kyusyu: { title: "九州", prefectures: [], isFaded: true, buttonColor: "#FFABCE" }
     },
-    computed: {
-        fadeClasses() {
-          return {
-            'fade-in': this.isFaded,
-          };
-        },
-      },
-    methods:{
-        redirectTo(url){
-            window.location.href="./"+url+".html";
-        },
-        toggleFade(key) {
-          this.regions[key].isFaded = !this.regions[key].isFaded;
-      },
-    }
-})
+    titleArray:['hokkaido','kanto','tyubu','kinki','tyugoku','shikoku','kyusyu'],
+  },
+  computed: {
+    fadeClasses() {
+      return {
+        'fade-in': this.isFaded,
+      };
+    },
+  },
+  methods: {
+    async fetchPrefecturesData() {
+      const url = "../../back/src/get_prefectures.php";
+      const timestamp = new Date().getTime();
+
+      try {
+        const response = await axios.get(`${url}?timestamp=${timestamp}`);
+        return response.data;
+      } catch (error) {
+        console.error("データの取得に失敗しました。", error);
+        throw error;
+      }
+    },
+    setPrefecturesData(prefecturesData) {
+      Object.keys(prefecturesData).forEach((regionKey) => {
+        const regionData = prefecturesData[regionKey];
+        const prefecturesArray = JSON.parse(regionData.prefectures);
+
+        // 未定義の場合は新しいオブジェクトを作成する
+        if (!this.regions[this.titleArray[regionKey]]) {
+          this.$set(this.regions, regionKey, {});
+          console.log(this.titleArray[regionKey]);
+        }
+
+        // データの設定
+        this.$set(this.regions[this.titleArray[regionKey]], 'prefectures', prefecturesArray.map(item => item.prefecture_name));
+        console.log(this.regions[this.titleArray[regionKey]]);
+      });
+    },
+    async fetchPrefectures() {
+      try {
+        const prefecturesData = await this.fetchPrefecturesData();
+        this.setPrefecturesData(prefecturesData);
+      } catch (error) {
+        console.error("データの設定に失敗しました。", error);
+      }
+    },
+    redirectTo(url) {
+      window.location.href = `./${url}.html`;
+    },
+    toggleFade(regionKey) {
+      // リアクティブなプロパティの切り替え
+      this.$set(this.regions[regionKey], 'isFaded', !this.regions[regionKey]?.isFaded);
+    },
+  },
+  created() {
+    this.fetchPrefectures();
+  },
+});
