29a52adcb682050ab1d3dede6efd07835b2ec782
matsumotorinsu <115598562+matsumotorinsu@users.noreply.github.com>
2023-12-14 10:35:59 +0900

    都道府県画像の追加

--- Login/src/main/java/com/example/demo/form/UserForm.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.form;
-
-import lombok.Data;
-
-@Data
-public class UserForm {
-	private String user_id;
-	private String password;
-}
--- Login/src/main/java/com/example/demo/repository/UserCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entity.User;
-
-public interface UserCrudRepository extends CrudRepository<User, String> {
-
-}
